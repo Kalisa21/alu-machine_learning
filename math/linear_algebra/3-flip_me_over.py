26d3a02e63a1269fb5234dd758c41d69426c5ef4
Willy kalisa <127384315+Kalisa21@users.noreply.github.com>
2024-09-13 21:34:53 +0000

    LA

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,13 +1,43 @@
 #!/usr/bin/env python3
-"""
-    flips a 2D matrix over its main diagonal.
-"""
+'''
+    A function def cat_matrices(mat1, mat2, axis=0)
+    that concatenates two matrices along a specific axis
+'''
 
 
-def matrix_transpose(matrix):
+def matrix_shape(matrix):
     """
-    matrix: matrix to be transposed
+        Get the matrix shape
     """
-    trans_matrix = [[matrix[j][i] for j in range(len(matrix))]
-                    for i in range(len(matrix[0]))]
-    return
+    matrix_shape = []
+    while (type(matrix) is list):
+        matrix_shape.append(len(matrix))
+        matrix = matrix[0]
+    return matrix_shape
+
+
+def cat_matrices(mat1, mat2, axis=0):
+    """
+        concatenate a matrix
+    """
+    from copy import deepcopy
+    shape1 = matrix_shape(mat1)
+    shape2 = matrix_shape(mat2)
+    if len(shape1) != len(shape2):
+        return None
+    for i in range(len(shape1)):
+        if i != axis:
+            if shape1[i] != shape2[i]:
+                return None
+    return rec(deepcopy(mat1), deepcopy(mat2), axis, 0)
+
+
+def rec(m1, m2, axis=0, current=0):
+    """
+        Do some reclusive calling
+    """
+    if axis != current:
+        return [rec(m1[i], m2[i], axis, current + 1) for i in range(len(m1))]
+    m1.extend(m2)
+    return m1
+    
